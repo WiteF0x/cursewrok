60616b4b40624fe175e31a613b55f85ab28ec040
tommys <tommys@redwerk.com>
2018-11-29 20:48:06 +0300

    Console log of products is normal

--- src/components/Bin/index.js
+++ src/components/Bin/index.js
@@ -4,42 +4,39 @@ import './style.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default class Bin extends Component {
-    render() {
-        // constructor(props) {
-        //     super(props)
-        //     state = {
-
-        //     }
-        // }
-        var arr = [];
-        var mass = [
-            {
-            sales : {
-            title: "",
-            cena: ""
+    constructor(props) {
+            super(props)
+            this.state = {
+                title:"",
+                cena:""
             }
-    }
-    ]
+        }
+    render() {
+        
+        var arr = [];
+        var prod = [
+            {
+            sa : {
+            title:null,
+            cena: null,
+            }
+        }
+        ];
         for (var i = 0; i <localStorage.length; i++) {
             arr[i] = localStorage.key(i);
-            console.log('--mas[i]--',mass[i])
-            // mass[i].sales.title=JSON.parse(localStorage.getItem(arr[i])).title
-            // sales[i].cena=JSON.parse(localStorage.getItem(arr[i].cena))
-            // console.log('Наименование: ',mass[i].sales.title)
-            // console.log('Цена: ',sales[i].cena)
+            prod[i] = JSON.parse(localStorage.getItem(arr[i]))
+            console.log('Наименование: ', prod[i].title)
         }
-        // const listSales = mass.map((id) => <li>{id.title}</li>);
+        console.log('key massiv: ',arr)
+        console.log('product massiv: ',prod)
+        // const listSales = mass.map((id) => <li>{id}</li>);
             return (
                      <div className="container">
                         <div className="jumbotron">
                             <Link to='/'><img className="home-logo" alt="home" src="/img/home.png" /></Link>
                             {/* <ol>{listSales}</ol> */}
-                            {/* <button onCLick = {this.DeleteCLick}>Delete</button> */}
                         </div>
                     </div>
             )
         }
-        // DeleteClick = () => {
-        //     Storage.clear();
-        // }
     }
