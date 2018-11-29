30011d4e0f03b79e68fd0f739d705903698c59ea
tommys <tommys@redwerk.com>
2018-11-29 18:21:59 +0300

    Test push

--- src/components/Bin/index.js
+++ src/components/Bin/index.js
@@ -5,6 +5,12 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 export default class Bin extends Component {
     render() {
+        // constructor(props) {
+        //     super(props)
+        //     state = {
+
+        //     }
+        // }
         var arr = [];
         var mass = [
             {
@@ -14,9 +20,9 @@ export default class Bin extends Component {
             }
     }
     ]
-        for (var i = 0; i < localStorage.length; i++) {
+        for (var i = 0; i <1; i++) {
             arr[i] = localStorage.key(i);
-            mass[i].sales.title=JSON.parse(localStorage.getItem(arr[i]))
+            mass[i].sales.title=JSON.parse(localStorage.getItem(arr[i])).title
             // sales[i].cena=JSON.parse(localStorage.getItem(arr[i].cena))
             console.log('Наименование: ',mass[i].sales.title)
             // console.log('Цена: ',sales[i].cena)
