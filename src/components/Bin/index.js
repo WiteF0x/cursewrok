e2777bab202121c3aadd50452fabea5f12bd32e1
tommys <tommys@redwerk.com>
2018-11-29 17:58:05 +0300

    completely output parse elements

--- src/components/Bin/index.js
+++ src/components/Bin/index.js
@@ -6,25 +6,30 @@ import 'bootstrap/dist/css/bootstrap.css'
 export default class Bin extends Component {
     render() {
         var arr = [];
-        var sales = [];
+        var mass = [
+            {
+            sales : {
+            title: null,
+            cena: null
+        }
+    }
+    ]
         for (var i = 0; i < localStorage.length; i++) {
             arr[i] = localStorage.key(i);
-            sales[i]=localStorage.getItem(arr[i])
-            console.log(arr[i])
-            console.log(sales[i])
+            mass[i].sales.title=JSON.parse(localStorage.getItem(arr[i])).title
+            mass[i].sales.cena=JSON.parse(localStorage.getItem(arr[i])).cena
+            console.log('Наименование: ',mass[i].sales.title)
+            console.log('Цена: ',mass[i].sales.cena)
         }
-        const listSales = sales.map((id) => <li>{id}</li>);
+        // const listSales = mass.map((id) => <li>{id}</li>);
             return (
-                <div>
                      <div className="container">
                         <div className="jumbotron">
                             <Link to='/'><img className="home-logo" alt="home" src="/img/home.png" /></Link>
-                            <ol>{listSales}</ol>
+                            {/* <ol>{listSales}</ol> */}
                             {/* <button onCLick = {this.DeleteCLick}>Delete</button> */}
                         </div>
                     </div>
-
-                </div>
             )
         }
         // DeleteClick = () => {
